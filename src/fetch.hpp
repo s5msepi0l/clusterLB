9b186b8661b7ad6ebff2fc697b1e63857169ba01
sam sepiol <65565795+s5msepi0l@users.noreply.github.com>
2023-05-19 01:22:26 +0300

    -added shit

--- src/fetch.hpp
+++ src/fetch.hpp
@@ -2,6 +2,10 @@
 #include <fstream>
 #include <cstring>
 #include <string>
+#include <chrono>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <algorithm>
 
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -22,57 +26,79 @@ static int optval = 1;
 	amount of time in order in increase performance
 */
 
-//returns file content's, need's to be deallocated
-char *fetch(std::string path)
+//returns file content's, need's to be deallocated manually.
+//Memory hogging piece of shit
+char *fetch(char *path)
 {
 	std::ifstream file(path, std::ios::in|std::ios::binary|std::ios::ate);
+	std::string buf, tmp;
+	char *membuf = nullptr;
 	if (file.is_open())
 	{
 		file.seekg(0, std::ios::end);
 		uint_128 size = (file.tellg() * sizeof(char));
-		char *membuf = new char[]
+		file.seekg(0, std::ios::beg);
+
+		while(file)
+		{
+			std::getline(file, tmp);
+			buf.append(tmp);
+		}
+
+		char *membuf = new char[size + 1];
+		std::memcpy(membuf, buf.c_str(), size);
+		membuf[size] = '\0';
+		
+		return membuf;
 	}
 
-	return membuf;
+	return NULL;
 }
 
 class n_src_cli
 {
 public:
-	n_src_cli(std::string source, unsigned short port, uint_128 s_expiration) : addr(inet_addr(source.c_str())), port(port), s_expiration(s_expiration)
+	n_src_cli(std::string source, unsigned short port) : port(port)
 	{
 		this->socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
 		this->server.sin_family = AF_INET;
 		this->server.sin_port = htons(port);
-		this->server.sin_addr.s_addr = addr;
+		this->server.sin_addr.s_addr = inet_addr(source.c_str());
 
 		if (setsockopt(this->socket_fd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(int)) < 0) 
 			throw std::runtime_error("unable to reuse db port\n");
-		
-		if (setsockopt(this->socket_fd, SOL_SOCKET, SO_RCVTIMEO, &this->tv, sizeof(this->tv)) < 0)
-			throw std::runtime_error("unable to setup socket timeout\n");
 	
 		if (connect(this->socket_fd, (struct sockaddr*)&this->server, sizeof(this->server)) < 0)
 			throw std::runtime_error("unable to connect to desired source\n");
+	
+		this->cache = NULL;
 	}
 
-	std::string get(std::string filename)
+	//seamless request source documents from loadbalancer 
+	char *get(std::string filename)
 	{
-		if (this->cache_content == filename && this->cache == NULL)
+		if (this->cache_content == filename)
 			return this->cache;
 
 		this->cache_content = filename;
 		if (send(this->socket_fd, (const void *)filename.c_str(), filename.size(), 0) < 0) {
 			if (reconnect(10) < 0)
-				return nullptr;
-		}
+				return NULL;
+		}else
+			return NULL;
 
 		char *tmp = new char[BUFFER_SIZE];
-		recv(socket_fd, tmp, BUFFER_SIZE-1, 0);
-
-		strcpy(this->cache, tmp);
-		this->cache_size = strlen(this->cache);
+		int recv_amt = recv(socket_fd, tmp, BUFFER_SIZE-1, 0);
+		if (recv_amt < 0) 
+		{
+			delete[] tmp;
+			return NULL;
+		}
+		tmp[recv_amt] = '\0';
+		this->cache_size = strlen(tmp);
+		this->cache = new char[strlen(tmp) + 1];
+		std::memcpy(&this->cache, tmp, this->cache_size);
 		delete[] tmp;
 
 		return this->cache;
@@ -80,26 +106,36 @@ public:
 
 	~n_src_cli()
 	{
-		delete[] this->cache;
+		std::cout << "cli destructor called\n";
+		delete[] this->cache; 
 		close(this->socket_fd);
 	}
 
 private:
 	unsigned long addr;
 	unsigned short port;
-	unsigned long s_expiration;
 
 	int socket_fd = -1;
 	struct sockaddr_in server;
 
 	struct timeval tv = {
-	 .tv_sec = 120 
+	 .tv_sec = 2 
 	};
 	
 	//chaching using c style string's for ease of use, conveniance and slight performance gains 
 	char *cache;
 	uint_128 cache_size;
 	std::string cache_content;
+
+	bool thread_running = true;
+	std::thread monitor;
+	void monitor_cache()
+	{
+		while (this->thread_running)
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(200));
+		}
+	}
 
 	int reconnect(int max_attempts)
 	{
@@ -121,26 +157,47 @@ class n_src_host
 public:
 	n_src_host(int port)
 	{
+		std::cout << "Constructor activated\n";
 		this->socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
 		this->server.sin_family = AF_INET;
 		this->server.sin_port = htons(port);
 
-		setsockopt(this->socket_fd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(int));
-		setsockopt(this->socket_fd, SOL_SOCKET, SO_RCVTIMEO, &this->tv, sizeof(this->tv));
-
+		//reuse port
+		//setsockopt(this->socket_fd, SOL_SOCKET, SO_RCVTIMEO, &this->tv, sizeof(this->tv));
+		setsockopt(this->socket_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int));
+		
 		bind(this->socket_fd, (struct sockaddr*)&this->server, sizeof(this->server));
 		listen(this->socket_fd, 1024);
+
+		this->n_accept = std::thread(&n_src_host::n_master_accept, this);
+		this->n_recv = std::thread(&n_src_host::n_master_recv, this);
+
+	}
+
+	void moo()
+	{
+		std::cout << "Moo\n";
+	}
+	//close concurrent threads
+	~n_src_host()
+	{
+		std::cout << "destructor activated\n";
+		this->mutexpool.lock();
+		this->thread_running = false;
+		this->mutexpool.unlock();
+	
+		if (this->n_accept.joinable())
+			this->n_accept.join();
+		
+		if (this->n_recv.joinable())
+			this->n_recv.join();
 	}
 
 private:
 	int socket_fd;
 	struct sockaddr_in server;
 	std::vector<int> nodes;
-
-	struct timeval tv = {
-		.tv_sec = 120 
-	};
 	
 	bool thread_running = true;
 	std::mutex mutexpool;
@@ -149,19 +206,91 @@ private:
 
 	void n_master_accept()
 	{
+		std::cout << "starting thread acpt\n";
 		int sock_buffer;
 		while (this->thread_running)
 		{
-			sock_buffer = accept(this->socket_fd, (struct sockaddr*)&this->server, (socklen_t*)sizeof(server));
-			
-			this->mutexpool.lock();
+			sock_buffer = accept(this->socket_fd, (struct sockaddr*)&this->server, (socklen_t*)sizeof(this->server));
+			std::cout << "client connected!\n";
 			this->nodes.push_back(sock_buffer);
-			this->mutexpool.unlock();
 		}
 	}
 
+	//listen for packets from nodes
 	void n_master_recv()
 	{
+		std::cout << "starting thread recv\n";
+		
+		// find the maximum socket descriptor value
+		int maxSocket = 0;
+		if (!this->nodes.empty())
+			maxSocket = *std::max_element(this->nodes.begin(), this->nodes.end());
+	
+		// create and initialize the file descriptor set
+		fd_set readSet;
+		FD_ZERO(&readSet);
 
+		// add all sockets to the file descriptor set
+		for (int i = 0; i<this->nodes.size(); i++)
+		{
+			FD_SET(this->nodes[i], &readSet);
+		}
+		
+		struct timeval timeout;
+		timeout.tv_sec = 1;
+		timeout.tv_usec = 0;
+
+
+		this->mutexpool.lock();
+		while (this->thread_running)
+		{
+			this->mutexpool.unlock();
+			std::this_thread::sleep_for(std::chrono::milliseconds(200)); //sleep så he it slösar cpu cycles
+			// make a copy of the file descriptor set, as `select` modifies it
+			fd_set tempSet = readSet;
+
+			// call select to check for readable sockets
+			this->mutexpool.lock();
+
+			// piece of shit, overcomplicated blocks for absolutely no goddamn reason,
+			// tempted to just use threading paired with recvtimeout
+			int activity = select(maxSocket + 1, &tempSet, nullptr, nullptr, &timeout);
+				std::cout << "Hello world\n";
+			if (activity == -1)
+			{
+				throw std::runtime_error("something broke");
+				break;
+			}
+			this->mutexpool.unlock();
+
+			//no activity within the specified timeout period
+			if (activity == 0)
+				continue;
+
+			//clunky but gotta get this shit done like yesterday
+			for (int i = 0; i<this->nodes.size(); i++)
+			{
+				if (FD_ISSET(this->nodes[i], &tempSet))
+				{
+					char buffer[512];
+					std::memset(buffer, 0, sizeof(buffer));
+
+					int bytesRead = recv(this->nodes[i], buffer, sizeof(buffer) - 1, 0);
+					if (bytesRead == -1)
+					{
+						throw std::runtime_error("recv error");
+					}
+					else //something was recv'd
+					{
+						char *buf = fetch(buffer);
+						std::cout << "responding: " << buffer << " on socket: " << this->nodes[i] << "\n";
+						send(this->nodes[i], buf, strlen(buf)+1, 0);
+
+						delete[] buf;
+					}
+				}
+			}
+			this->mutexpool.lock();
+		}
 	}
 };
